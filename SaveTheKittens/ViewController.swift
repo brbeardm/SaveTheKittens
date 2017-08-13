2f2c1bdbf04ce5ad5411edc4c3535f23e473df79
Brian Beardmore <bbeardmore@carvizor.com>
2017-08-13 17:46:43 -0500

    this rules...

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        print("hello little kitty cat...")
+    
     }
     
-    print("hello little kitty")
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
