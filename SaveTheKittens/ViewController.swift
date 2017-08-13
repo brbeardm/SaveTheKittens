b565b5f385d3f2705b8ce55e565c845aa64396a5
Brian Beardmore <bbeardmore@carvizor.com>
2017-08-13 10:20:18 -0500

    first commit

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    print("hello little kitty")
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
